d6144c81437b0b8ae58bc275f9c02e0adda500dd
libstr <libinjoseph18@gmail.com>
2016-01-17 23:46:55 +0530

    main

---
+++ About/CYRIL OFFICIAL WEBSITE_files/cyril.js
@@ -0,0 +1,128 @@
+// JavaScript Document
+
+ (function($){
+  $.fn.instagram = function(options) {
+    var that = this,
+        apiEndpoint = "https://api.instagram.com/v1",
+        settings = {
+            hash: null
+          , userId: null
+          , locationId: null
+          , search: null
+          , accessToken: null
+          , clientId: null
+          , show: null
+          , onLoad: null
+          , onComplete: null
+          , maxId: null
+          , minId: null
+          , next_url: null
+        };
+        
+    options && $.extend(settings, options);
+    
+
+    function createPhotoElement(photo) {
+      return $('<div>')
+      	.append('<div class="layoutInstagramTxt"><img src="'+ photo.user.profile_picture +'"width="40px"></div>' + '<div class="layoutInstagramTxtName">'+ photo.user.username+'</br>'+''+'</div>')
+        .addClass('instagram-placeholder')
+        .attr('id', photo.id)
+        .prepend(
+          $('<a>')
+            .attr('target', '_blank')
+            .attr('href', photo.link)
+            .append(
+              $('<img>')
+                .addClass('instagram-image')
+                .attr('src', photo.images.low_resolution.url)
+            )
+        );
+		
+		
+		
+    }
+    
+    function createEmptyElement() {
+      return $('<div>')
+        .addClass('instagram-placeholder')
+        .attr('id', 'empty')
+        .append($('<p>').html('No photos for this query'));
+    }
+    
+    function composeRequestURL() {
+
+      var url = apiEndpoint,
+          params = {};
+      
+      if (settings.next_url != null) {
+        return settings.next_url;
+      }
+
+      if(settings.hash != null) {
+        url += "/tags/" + settings.hash + "/media/recent";
+      }
+      else if(settings.search != null) {
+        url += "/media/search";
+        params.lat = settings.search.lat;
+        params.lng = settings.search.lng;
+        settings.search.max_timestamp != null && (params.max_timestamp = settings.search.max_timestamp);
+        settings.search.min_timestamp != null && (params.min_timestamp = settings.search.min_timestamp);
+        settings.search.distance != null && (params.distance = settings.search.distance);
+      }
+      else if(settings.userId != null) {
+        url += "/users/" + settings.userId + "/media/recent";
+      }
+      else if(settings.locationId != null) {
+        url += "/locations/" + settings.locationId + "/media/recent";
+      }
+      else {
+        url += "/media/popular";
+      }
+      
+      settings.accessToken != null && (params.access_token = settings.accessToken);
+      settings.clientId != null && (params.client_id = settings.clientId);
+      settings.minId != null && (params.min_id = settings.minId);
+      settings.maxId != null && (params.max_id = settings.maxId);
+
+      url += "?" + $.param(params)
+      
+      return url;
+    }
+    
+    settings.onLoad != null && typeof settings.onLoad == 'function' && settings.onLoad();
+    
+    $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      cache: false,
+      url: composeRequestURL(),
+      success: function(res) {
+        var length = typeof res.data != 'undefined' ? res.data.length : 0;
+        var limit = settings.show != null && settings.show < length ? settings.show : length;
+        
+        if(limit > 0) {
+          for(var i = 0; i < limit; i++) {
+            that.append(createPhotoElement(res.data[i]));
+          }
+        }
+        else {
+          that.append(createEmptyElement());
+        }
+
+        settings.onComplete != null && typeof settings.onComplete == 'function' && settings.onComplete(res.data, res);
+      }
+    });
+    
+    return this;
+  };
+})(jQuery);
+
+$(document).ready(function() {
+
+$(".instagramApi").instagram({
+    	accessToken: "2149979.7e449ef.3c37b13a02824f92957a50ef6841aabf",
+		userId: "182330499", 
+        show: 8,
+        clientId: '7e449ef40b4d4c89a73c9c5a8646ecbc'
+      });
+});
